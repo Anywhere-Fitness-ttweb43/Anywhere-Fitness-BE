e8855a9fad35cd34831c09a494c739cc1a6b5240
Nicholas Smith <nicholassmithbus@gmail.com>
2021-03-01 11:25:16 -0700

    removed app.css, added HomePage.js component, added styledcomponents dependency

--- js-front-end/src/App.js
+++ js-front-end/src/App.js
@@ -3,13 +3,14 @@ import { Route } from "react-router-dom";
 import Login from "./components/Login";
 import GetUserInfo from "./components/UserInfo";
 import ProtectedRoute from "./components/ProtectedRoute";
-import "./App.css";
+import HomePage from './components/HomePage'
 
 function App() {
 	return (
-		<div className="App">
-			<Route exact path="/" component={Login} />
-			<ProtectedRoute exact path="/userinfo" component={GetUserInfo} />
+		<div className="App" style={ {padding: 0, margin: 0} }>
+			{/* <Route exact path="/" component={Login} />
+			<ProtectedRoute exact path="/userinfo" component={GetUserInfo} /> */}
+			<HomePage />
 		</div>
 	);
 }

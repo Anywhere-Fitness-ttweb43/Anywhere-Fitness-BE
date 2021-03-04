ffdeb209dce26cc78c810688eadfeba684354de4
Anas Abdelsalam <anas.abdelsalam57@gmail.com>
2021-03-04 16:25:10 -0600

    updated NewClassForm

--- js-front-end/src/validation/classSchema.js
+++ js-front-end/src/validation/classSchema.js
@@ -3,7 +3,8 @@ import * as yup from "yup";
 const classSchema = yup.object().shape({
 
     name: yup.string()
-        .required("Name is a required field"),
+        .required("Name is a required field")
+        .min(2, "Minimum of 2 Characters for the Name"),
 
     sTime: yup.number()
         .required("Start Time is a required field"), 
@@ -11,9 +12,13 @@ const classSchema = yup.object().shape({
     duration: yup.number()
         .required("Duration is a required field"),
 
-    date: yup.string()
+    date: yup.date()
         .required("Date is a required field")
-        .matches("\d{2}/\d{2}/\d{4}"),
+        .test("is-one-week","Date needs to be set a week in advance",(date)=>{
+            const cutoff = new Date()
+            cutoff.setDate(cutoff.getDate()+6)
+            return cutoff <= date 
+        }),
 
     type: yup.string()
         .required("Type is a required field"), 
